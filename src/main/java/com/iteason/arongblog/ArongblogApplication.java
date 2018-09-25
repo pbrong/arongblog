fc80ef8b6ab4214e6695ba6ba3b756b2908f69a6
1378789620@qq.com <1378789620@qq.com>
2018-09-25 09:49:49 +0800

    初始化项目

---
+++ src/main/java/com/iteason/arongblog/ArongblogApplication.java
@@ -0,0 +1,12 @@
+package com.iteason.arongblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArongblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArongblogApplication.class, args);
+	}
+}
---
+++ src/test/java/com/iteason/arongblog/ArongblogApplicationTests.java
@@ -0,0 +1,16 @@
+package com.iteason.arongblog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArongblogApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
